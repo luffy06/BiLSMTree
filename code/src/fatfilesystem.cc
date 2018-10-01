06196ebb8c25119c47d39ece3da709f1b374bc3d
luffy06 <534427411@qq.com>
2018-10-01 14:57:28 +0800

    update code

---
+++ code/src/fatfilesystem.cc
@@ -0,0 +1,5 @@
+#include "fatfilesystem.h"
+
+virtual FatFileSystem::FatFileSystem() {
+
+}
--- code/src/include/fatfilesystem.h
+++ code/src/include/fatfilesystem.h
@@ -2,13 +2,24 @@
 
 class FatFileSystem : public FileSystem {
 public:
-  FatFileSystem();
-  ~FatFileSystem();
+  virtual FatFileSystem();
+  virtual ~FatFileSystem();
 
+  virtual int open(const std::string &filename, const int &mode) ;
+
+  virtual void read(const int &file_number, std::string &data);
+
+  virtual void write(const int &file_number, const std::string &data);
+
+  virtual void close(const int &file_number);
+
+  virtual void seek(const int &file_number, const int &offset);
+
+  virtual int tell();
   
 private:
   const int MAX_BLOCK_NUMS = (1 << 16) * 4;
   const int CLUSTER_NUMS = 64;
 
-
+  int *fat;
 };
