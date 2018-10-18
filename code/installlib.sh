1e1cece7d0a70867c7bab4fc686e8d6cb43124cc
wushangyu <534427411@qq.com>
2018-10-18 11:16:18 +0800

    update lib

--- code/installlib.sh
+++ code/installlib.sh
@@ -1,16 +1,14 @@
 rm -rf lib/
 mkdir lib
 cd lib/
-rm -rf leveldb/
-rm -rf threadpool/
-rm -rf HdrHistogram_c/
-git clone https://github.com/google/leveldb
-cd leveldb/
-mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
+# git clone https://github.com/google/leveldb
+# cd leveldb/
+# mkdir -p build && cd build
+# cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
 cd ../../
 wget http://prdownloads.sourceforge.net/threadpool/threadpool-0_2_5-src.zip
 tar -zxvf threadpool-0_2_5-src.zip
 mv threadpool-0_2_5-src/threadpool/boost threadpool
 rm -rf threadpool-0_2_5-src threadpool-0_2_5-src.zip
 git clone https://github.com/HdrHistogram/HdrHistogram_c
+git clone https://github.com/brianfrankcooper/YCSB
