497dd0c429ae565aad660877d97a807ec034a0ae
luffy06 <534427411@qq.com>
2018-11-18 21:32:58 +0800

    fix bugs

--- code/src/include/cuckoofilter.h
+++ code/src/include/cuckoofilter.h
@@ -212,8 +212,9 @@ private:
 
   Slice GetFingerPrint(const Slice key) {
     size_t f = Hash(key.data(), key.size(), FPSEED) % 255 + 1;
+    std::string f_str = Util::IntToString(f);
     // std::cout << "Key:" << key.ToString() << "\tFP:" << f << std::endl;
-    return Slice(Util::IntToString(f));
+    return Slice(f_str.data(), f_str.size());
   }
 
   Info GetInfo(const Slice key) {
