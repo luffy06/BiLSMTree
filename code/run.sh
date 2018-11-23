e65c437878c212291b8062eb51a27f8a54ba653d
wushangyu <534427411@qq.com>
2018-11-24 01:12:56 +0800

    update script

---
+++ code/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+datafolder="data"
+resultfolder="result"
+suffix=".in"
+algos=('LevelDB')
+for algo in ${algos[*]}; do
+  if [[ -f 'config.in' ]]; then
+    rm 'config.in'
+  fi
+  echo ${algo} >> config.in
+  resultname=${resultfolder}/${algo}.out
+  if [[ -f ${resultname} ]]; then
+    rm ${resultname}
+  fi
+  echo 'Running '${algo} 
+  for file in ${datafolder}/*${suffix}; do
+    filename=`basename $file`
+    echo 'DATA '${filename}
+    echo 'RUNNING '${filename} >> ${resultname}
+    echo `build/main` < ${datafolder}/${filename}  >> ${resultname}
+  done
+done
+echo 'LOADING RESULT'
+# echo `python3 loadresult.py`
