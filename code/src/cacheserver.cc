38928e9e764239852862a9e7a1b07e31ebe88620
wushangyu <534427411@qq.com>
2018-11-30 16:12:10 +0800

    fix algorithm bugs

--- code/src/cacheserver.cc
+++ code/src/cacheserver.cc
@@ -15,40 +15,53 @@ CacheServer::~CacheServer() {
 }
 
 SkipList* CacheServer::Put(const KV kv) {
-  KV pop_kv;
+  std::string algo = Util::GetAlgorithm();
   SkipList* res = NULL;
-  if (lru_->Put(kv, pop_kv)) {
-    // std::cout << "POP KV:" << pop_kv.key_.ToString() << std::endl;
-    // lru2q is full
-    if (mem_->IsFull()) {
-      // std::cout << "Memtable is full Immutable Memtable Size:" << imm_size_ << std::endl;
-      // immutable memtable is full
-      if (imms_.size() == Config::CacheServerConfig::MAXSIZE) {
-        // std::cout << "DUMP" << std::endl;
-        // the size of immutable memtables is full
-        // record tail position
-        size_t min_fre = imms_[0].fre_;
-        size_t index = 0;
-        for (size_t i = 1; i < imms_.size(); ++ i) {
-          if (imms_[i].fre_ < min_fre) {
-            min_fre = imms_[i].fre_;
-            index = i;
+  if (algo == std::string("BiLSMTree")) {
+    KV pop_kv;
+    if (lru_->Put(kv, pop_kv)) {
+      // lru2q is full
+      if (mem_->IsFull()) {
+        // immutable memtable is full
+        if (imms_.size() == Config::CacheServerConfig::MAXSIZE) {
+          // the size of immutable memtables is full
+          // record tail position
+          size_t min_fre = imms_[0].fre_;
+          size_t index = 0;
+          for (size_t i = 1; i < imms_.size(); ++ i) {
+            if (imms_[i].fre_ < min_fre) {
+              min_fre = imms_[i].fre_;
+              index = i;
+            }
           }
+          res = imms_[index].imm_;
+          imms_.erase(imms_.begin() + index);
         }
-        res = imms_[index].imm_;
-        imms_.erase(imms_.begin() + index);
+        mem_->DisableWrite();
+        // create a new listnode
+        imms_.push_back(ListNode(mem_));
+        // create a new immutable memtable
+        mem_ = new SkipList();
+      }
+      // insert memtable
+      mem_->Insert(pop_kv);
+    }
+  }
+  else if (algo == std::string("LevelDB") || algo == std::string("LevelDB-KV")) {
+    if (mem_->IsFull()) {
+      if (imms_.size() != 0) {
+        res = imms_[0].imm_;
+        imms_.clear();
       }
       mem_->DisableWrite();
-      // create a new listnode
-      ListNode p = ListNode(mem_);
-      imms_.push_back(p);
-      // create a new immutable memtable
+      imms_.push_back(ListNode(mem_));
       mem_ = new SkipList();
     }
-    // std::cout << "Insert in Mem" << std::endl;
-    // insert memtable
     mem_->Insert(pop_kv);
-    // std::cout << std::string(10, '@') << std::endl;
+  }
+  else {
+    std::cout << "Algorithm Error" << std::endl;
+    assert(false);
   }
   return res;
 }
