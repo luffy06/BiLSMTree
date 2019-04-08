c1cf14d7aca810ed60b3b49532c0dc0287a88628
wushangyu <534427411@qq.com>
2019-04-09 02:21:12 +0800

    remove const

--- code/src/include/lru2q.h
+++ code/src/include/lru2q.h
@@ -27,8 +27,6 @@ public:
 private:
   BiList *lru_;
   BiList *fifo_;
-  const std::vector<std::string> lru2q_imm_algos = {"BiLSMTree", "BiLSMTree-Ext"};
-  const std::vector<std::string> lru2q_algos = {"BiLSMTree-Direct"};
 };
 
 }
